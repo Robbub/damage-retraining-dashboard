cac2a3917eb58cf19ce84b8b8dce2bd35ee5e9b7
Robert Estreyus Suba <robertbubsuba@gmail.com>
2026-04-28 14:41:50 +0800

    Remove env secrets

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmu26lOiolnAXAmN7j2feA7xuKJLsGJkg",
-  authDomain: "crack-classification-dashboard.firebaseapp.com",
-  projectId: "crack-classification-dashboard",
-  storageBucket: "crack-classification-dashboard.firebasestorage.app",
-  messagingSenderId: "1077608675636",
-  appId: "1:1077608675636:web:403d4bde6fe46b506a0d61"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
